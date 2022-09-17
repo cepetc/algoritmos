4c7c17df53fab84ce4172968837a14efc1bea6af
João Cláudio <105642920+joaoclaudiomm@users.noreply.github.com>
2022-09-17 18:27:50 -0300

    Add files via upload
    exercicio falho

---
+++ calcularidade2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int num1, num2, idade1, idade2;
+	printf("\n informe sua idade:");
+	scanf("%d",&num1);
+	printf("\n informe sua idade:");
+	scanf("%d",&num2);
+	idade1=(num1*num1);
+	idade2=num2/num2;
+	printf("\n a primeira idade tera:%d%d");
+	
+	
+	
+	return(0);
+
+}
