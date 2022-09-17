5e41a7bc5a9850c629630d922313c82d707d4dfd
joao.miranda <joao.mirando1607@gmail.com>
2022-09-17 20:14:34 -0300

    Criação dos arquivos para calculadoras.

---
+++ reajuste.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int num, reaj, R;
+	
+	printf("\n coloque um valor:");
+	scanf("%d",&num);
+	reaj=10/100;
+	R=(num/reaj);
+	printf("\n o valor depois do reajuste sera:%d");
+	return(0);
+}
