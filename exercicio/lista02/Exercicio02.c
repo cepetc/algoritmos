1cb2d829ccf813ea945a79eaac0ebcf5b7727c74
joao.miranda <joao.mirando1607@gmail.com>
2022-09-17 20:59:15 -0300

    Criação do código fonte dos exercícios 01, 02 e 03.

--- exercicio/lista02/Exercicio02.c
+++ exercicio/lista02/Exercicio02.c
@@ -1,16 +1,28 @@
+// Faça um programa que receba a idade de duas pessoas e mostre o dobra da idade da primeira e a metade da idade da segunda.
+
 #include<stdio.h>
 main(){
-	int num1, num2, idade1, idade2;
-	printf("\n informe sua idade:");
-	scanf("%d",&num1);
-	printf("\n informe sua idade:");
-	scanf("%d",&num2);
-	idade1=(num1*num1);
-	idade2=num2/num2;
-	printf("\n a primeira idade tera:%d%d");
+	// Declara as variaveis.
+	float idade1, idade2, resultadoDobro, resultadoMetade;
 	
+	// Faz a leitura da primeira idade.
+	printf("Informe o valor da primeira idade: \n");
+	scanf("%f",&idade1);
 	
+	// Faz a leitura da segunda idade.
+	printf("Informe o valor da segunda idade: \n");
+	scanf("%f",&idade2);
 	
+	// Calcular o dobro da primeira idade.
+	resultadoDobro=(idade1*2);
+
+	// Calcular a metade da segunda idade.
+	resultadoMetade=(idade2/2);
+
+	// imprime os resultados.
+	printf("O dobro da primeira idade: %.2f \n",resultadoDobro);
+	printf("A metade da segunda idade: %.2f \n",resultadoMetade);
+		
 	return(0);
 
 }
--- exercicio/lista02/Exercicio03.c
+++ exercicio/lista02/Exercicio03.c
@@ -1,12 +1,19 @@
+// Receber um valor inteiro do teclado e imprimir esse valor com reajuste de 10%.
+
 #include<stdio.h>
 main()
 {
-	int num, reajuste, R;
+	// Declara as variaveis.
+	int valor, resultado;
 	
-	printf("\n coloque um valor:");
-	scanf("%d",&num);
-	reaj=10/100;
-	R=(num/reaj);
-	printf("\n o valor depois do reajuste sera:%d");
+	// Recebe o valor a ser reajustado em 10%.
+	printf("Digitar o valor a ser reajustado: \n");
+	scanf("%d",&valor);
+	
+	// Calcula o reajusta de 10% em cima do valor recebido como parâmetro.
+	resultado = (valor * 0.10);
+	
+	// Imprime o resultado reajustado.
+	printf("O valor reajustado e: %d \n", resultado);
 	return(0);
 }
