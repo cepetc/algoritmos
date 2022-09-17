11a583d6a1390d67836246493c9a1826e5fdb1d0
João Cláudio <105642920+joaoclaudiomm@users.noreply.github.com>
2022-09-17 19:18:35 -0300

    Add files via upload

---
+++ media.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	float num1, num2, num3, media;
+	 printf("\n informe os trez numeros:");
+	 scanf("%f%f%f",&num1,&num2,&num3);
+	 media=num1 / 3;
+	 media= num2 / 3;
+	 media= num3 / 3;
+	 printf("\n sua media sera: %f");
+	 
+	 
+	return(0);
+}
